8da2d37552529199cdcb7433d6218fcdf9ce1cfb
Max000 <kershevmaxim@gmail.com>
2025-05-29 16:46:44 +0900

    Добавленна БД в Websocket

---
+++ TestWSServer/models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+import datetime
+
+# Подключение к БД
+engine = create_engine('sqlite:///./clicker.db', connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Модель пользователя
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True)
+    total_clicks = Column(Integer, default=0)
+
+# Модель сессии
+class SessionLog(Base):
+    __tablename__ = "sessions"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    start_time = Column(DateTime, default=datetime.datetime.utcnow)
+    end_time = Column(DateTime)
+    clicks = Column(Integer)
+
+# Инициализация БД
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
